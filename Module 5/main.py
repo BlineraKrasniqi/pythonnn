03aaccd9d01187a33c337417f7a4c7b7fe1c5fd7
Student <blinerakrasniqi797@gmail.com>
2024-11-30 11:38:53 +0100

    ...

---
+++ Module 5/main.py
@@ -0,0 +1,19 @@
+total = 0
+
+for number in range(1,11):
+    if number % 2 == 0:
+        total += number
+print("Shuma e numrave qift prej 1 deri ne 10 eshte:", total)
+
+def greet():
+    print("Hello World!!!")
+
+greet()
+
+def greet_person(name):
+    print("Hello", name)
+
+greet_person("Arteida")
+
+# result = add(3,8)
+#print("3+ 7=", result)
